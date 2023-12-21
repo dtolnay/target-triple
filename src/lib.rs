51c67e9b96bc57c7895a16d91122287d5f8742e7
David Tolnay <dtolnay@gmail.com>
2023-12-20 20:55:22 -0800

    Add crosslink icons to top of rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+//! [![github]](https://github.com/dtolnay/target-triple)&ensp;[![crates-io]](https://crates.io/crates/target-triple)&ensp;[![docs-rs]](https://docs.rs/target-triple)
+//!
+//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
+//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs
+
 #![doc(html_root_url = "https://docs.rs/target-triple/0.1.1")]
 
 include!(concat!(env!("OUT_DIR"), "/macros.rs"));
